8ce0229cf3071f2d48896cba599bc6740e27ac87
Simon Wessel <simon@nllptr.org>
2015-03-02 14:40:30 +0100

    Cleanyfied RSA.java

--- Project1/src/org/nllptr/RSA.java
+++ Project1/src/org/nllptr/RSA.java
@@ -1,7 +1,5 @@
 package org.nllptr;
 
-import sun.text.ComposedCharIter;
-
 import java.math.BigInteger;
 import java.util.Random;
 
@@ -12,24 +10,26 @@ public class RSA {
     private static final Random rnd = new Random();
 
     public static void main(String[] args) {
-//        generateHundred(512);
-        int bitLength = 512;
-        BigInteger p = generatePrime(bitLength);
-        BigInteger q = generatePrime(bitLength);
-        System.out.println(p + "\n" + q);
+//        int bitLength = 512;
+//        BigInteger p = generatePrime(bitLength);
+//        BigInteger q = generatePrime(bitLength);
+        BigInteger p = new BigInteger("7196783082013959922705252156453947032155512098110600241518532136595441314693077053581923961745331720607226687877041023096195136049660250158819636144589893");
+        BigInteger q = new BigInteger("7022039410784562750645367011775226028701070992603592054912743598591995911025455679529540299006713316856397262016000570070262232362624748700235036719333997");
+        System.out.println("P: " + p + "\nQ: " + q);
         BigInteger m = (p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE))).divide(TWO);
         BigInteger e = new BigInteger(String.valueOf(new Double(Math.pow(2, 16) + 1).longValue()));
-        System.out.println(m + "\n" + e);
+        System.out.println("M: " + m + "\nE: " + e);
         BigInteger d = inverseModM(e, m);
-        System.out.println(d);
+        System.out.println("D: " + d);
         BigInteger N = p.multiply(q);
-        BigInteger s = new BigInteger("42");
-        System.out.println(s);
+        BigInteger s = randomOddBigInteger(400);
+        System.out.println("S: " + s);
         BigInteger c = s.modPow(e, N);
-        System.out.println(c);
+        System.out.println("C: " + c);
         BigInteger z = c.modPow(d, N);
-        System.out.println(z);
-
+        System.out.println("Z: " + z);
+        System.out.println((z.equals(s) ? "OK!" : "INCORRECT!"));
+        testGenerateHundred(512);
     }
 
     public static void testInverseMod() {
@@ -38,13 +38,12 @@ public class RSA {
         System.out.println("5^-1 mod 67: expected 27. Got " + inverseModM(new BigInteger("5"), new BigInteger("67")));
     }
 
-    public static void generateHundred(int bitLength) {
+    public static void testGenerateHundred(int bitLength) {
         long startTime = System.currentTimeMillis();
-        BigInteger n = null;
 
         for (int i = 0; i < 100; i++) {
             generatePrime(bitLength);
-            System.out.print("\r[" + (i < 10 ? "0" + String.valueOf(i + 1) : String.valueOf(i + 1)) + "%] Computing primes...");
+            System.out.print("\r[" + (i < 10 ? "0" + String.valueOf(i + 1) : String.valueOf(i + 1)) + "%] Computing primes... ");
         }
         long runningTime = (System.currentTimeMillis() - startTime);
         System.out.print(" Finished in " + runningTime + " ms\n");
@@ -65,7 +64,7 @@ public class RSA {
             d2 = t3;
         }
         if(d1.compareTo(BigInteger.ONE) > 0)
-            return null;
+            return null; // a has no inverse mod M
         if(v1.compareTo(BigInteger.ZERO) < 0)
             return v1.add(m);
         else
@@ -76,8 +75,8 @@ public class RSA {
         if (n.compareTo(FOUR) < 0)
             return true;
         BigInteger s = n.subtract(BigInteger.ONE);
-        BigInteger nMinusOne = s; // Store value to avoid recomputation in loop.
-        BigInteger nMinusTwo = n.subtract(TWO); // Store value to avoid recomputation in loop.
+        BigInteger nMinusOne = s; // Store value to avoid re-computation in loop.
+        BigInteger nMinusTwo = n.subtract(TWO); // Store value to avoid re-computation in loop.
         int r = 0;
         while (s.mod(TWO).equals(BigInteger.ZERO)) {
             s = s.divide(TWO);
